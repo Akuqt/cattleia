4dca6da44f8b03ebab0701216e4b97b104cb3247
Aku-mi <bestor911@gmail.com>
2021-08-28 15:28:12 -0500

    Persist Storage Added
    Able to save and retrieve from local storage.

--- cattleia-native-app/App/index.tsx
+++ cattleia-native-app/App/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import {store} from './redux/store';
+import {store, persistor} from './redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
 import {Pages} from './pages';
 
 export default () => (
   <Provider store={store}>
-    <Pages />
+    <PersistGate loading={null} persistor={persistor}>
+      <Pages />
+    </PersistGate>
   </Provider>
 );
