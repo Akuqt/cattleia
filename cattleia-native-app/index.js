3b91ca328be20e6159580406a653b22f9e376edf
Aku-mi <bestor911@gmail.com>
2021-11-08 20:29:47 -0500

    App name bug
    App name runtime error fixed.

--- cattleia-native-app/index.js
+++ cattleia-native-app/index.js
@@ -1,5 +1,5 @@
-import {name as cattleiaName} from './cattleia.json';
+import {name as appName} from './app.json';
 import {AppRegistry} from 'react-native';
 import App from './App';
 
-AppRegistry.registerComponent(cattleiaName, () => App);
+AppRegistry.registerComponent(appName, () => App);
