99174d2f5c8ce9939b54957cf383e1e3051f26ea
Aku-mi <bestor911@gmail.com>
2021-11-12 22:29:24 -0500

    Jest ENV load
    Loads env within jest tests.

--- cattleia-backend/jest.config.js
+++ cattleia-backend/jest.config.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
+
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   rootDir: "./src/test/jest",
-  setupFiles: ["<rootDir>/setup.ts"],
   verbose: true,
   silent: true,
   detectOpenHandles: false,
